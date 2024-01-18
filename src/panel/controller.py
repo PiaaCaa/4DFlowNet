f969a1dba4e55e52decf6fe175bfac7076ae63ce
PiaaCaa <pia.callmer@home.se>
2024-01-18 10:54:24 +0100

    panel updates

--- src/panel/controller.py
+++ src/panel/controller.py
@@ -26,12 +26,47 @@ class PanelController(param.Parameterized):
     view = param.ClassSelector(class_=PanelDashboard)
     layout = pn.template.FastListTemplate()
 
-    def __init__(self, **params):
+    def __init__(self, model_name,hr_file, lr_file,pred_dir, pred_file, **params):
         super().__init__(**params)
-        self.model = Model()
-        self.view = PanelDashboard()
+        self.model = Evaluate_NNModel(model_name,hr_file, lr_file,pred_dir, pred_file,)
+        self.model.__post_init__()
+        self.view = PanelDashboard(self.model.lr_data, self.model.hr_data, self.model.pred_data)
+        
+        
+    
     
     def run(self):
         self.layout = self.view.get_layout()
         self.layout.servable()
         print("ruuuuuning")
+
+
+    @param.depends(
+            "view.psidebar.slicing_x",
+            "view.psidebar.slicing_y",
+            "view.psidebar.axis",
+            "view.psidebar.t_frame",
+            watch=True
+    )    
+    def recalculate(self):
+        print("ES PASSIERT WAS!!!")
+
+        #readout of widgets, e.g. slicing, axis, etc.
+        slicing_x = self.view.psidebar.slicing_x
+        slicing_y = self.view.psidebar.slicing_y
+        axis = self.view.psidebar.axis
+        t_frame = self.view.psidebar.t_frame
+
+        print(slicing_x, slicing_y, axis)
+
+        # TODO 
+        # recaluclate model and return updated data
+        self.model.update_calculation(t_frame, slicing_x,  axis)
+
+        # update view,esp. update plots
+        self.view.pplots.update_plots(self.model.lr_slice, self.model.hr_slice, self.model.pred_slice)
+        
+        # also check matplotlib responsive
+
+        self.view.update_layout() # TODO maybe delete this later
+        
--- src/panel/run_panel.py
+++ src/panel/run_panel.py
@@ -5,7 +5,15 @@ import param
 from controller import *
 
 
-controller = PanelController()
+model_name = '20240115-1535'
+hr_file = 'data/CARDIAC/M1_2mm_step2_static_dynamic.h5'
+lr_file = 'data/CARDIAC/M1_2mm_step2_static_dynamic.h5'
+pred_dir = 'results/predictions'
+pred_file = 'data/CARDIAC/M1_2mm_step2_static_dynamic.h5'
+
+
+
+controller = PanelController(model_name=model_name, hr_file=hr_file, lr_file=lr_file, pred_dir=pred_dir, pred_file=pred_file)
 controller.run()
 
 
