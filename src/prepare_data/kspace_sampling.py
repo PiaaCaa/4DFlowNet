7c9c0f1043855c428333b84d1777c7e0d44b097a
PiaaCaa <pia.callmer@home.se>
2024-03-22 16:47:51 +0100

    k space sampling extended

--- src/prepare_data/kspace_sampling.py
+++ src/prepare_data/kspace_sampling.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import h5functions
 import fft_downsampling as fft_fcts
-
+import cfl 
 
 # --------this code is copied from Alexander Fyrdahl:------
 def biot_savart_simulation(segments, locations):
@@ -463,13 +463,93 @@ def k_space_sampling_static(path_kmask, path_order, path_datamodel, save_as):
         h5functions.save_to_h5(save_as, f'venc_{vel}', interpolated_data[f'venc_{vel}'], expand_dims=False)
 
 
-if __name__ == '__main__':
-    # Define datasets
-    path_kmask = 'data/kspacemask.h5'
-    path_order = 'data/order.mat'
-    path_datamodel = 'data/CARDIAC/M2_2mm_step1_static_dynamic.h5'
-    save_as = 'results/interpolation/M2_2mm_step1_static_kspace_sampled_coilsens2.h5'
+def k_space_sampling_timeseries_new(path_order,data,save_as):
 
+    # load order data
+        # make batches to process data
+    
+    order = sio.loadmat(path_order)
+    phs_max = np.max(order['phs'])
+    Nset = np.max(order['set'])
+    lins = order['lin'].squeeze()
+    pars = order['par'].squeeze()
+    sets = order['set'].squeeze()
+    phss = order['phs'].squeeze()
+    print(  phs_max)
+
+    # get spatial shape of kspacemask
+    X = order['NCol'].squeeze()//2
+    Y = order['NLin'].squeeze()
+    Z = order['NPar'].squeeze()
+    # todo adjust to inout params
+    N_frames = phs_max
+    print(X, Y, Z, N_frames)
+    vel_set = {'u':0, 'v':1,'w':2}
+    set_vel = {'0':'u', '1':'v', '2':'w'}
+    
+
+    # reshape data to the same size as kspacemask
+    for vel in ['u', 'v', 'w']:
+        data[vel] = adjust_image_size(data[vel], (data[vel].shape[0], X, Y, Z))
+    
+    data['magnitude'] = np.ones_like(data['u'])
+
+    # t_range of mr k-space mask and cfd data
+    mr_range = np.linspace(0, 1, N_frames)
+    cfd_range = np.linspace(0, 1 , data['u'].shape[0]) 
+
+    print('Transforming velocity data into centered k space ..')
+    data_ksp = {}
+    for vel in ['u', 'v', 'w']:
+        data_ksp[vel] = np.zeros((data[vel].shape[0], X, Y, Z), dtype = np.complex64)
+        for t in range(data[vel].shape[0]):
+            data_ksp[vel][t] = fft_fcts.velocity_img_to_centered_kspace(data[vel][t], data['magnitude'][t], venc = data[f'venc_{vel}'][t])
+
+    #TODO optimize by make mapping only once for 0 -- phs_max -1
+    # make mapping only once
+
+    def find_t_int_idx(phs):
+        #find closest index in cfd data
+        idx = np.argmin(np.abs(cfd_range - mr_range[phs-1]))
+
+        return idx
+
+    print('Sample k space according to order ..')
+    sampled_kspace = np.zeros((N_frames, X, Y, Z, Nset), dtype = np.complex64)
+    for lin, par, phs, set_ in zip(lins, pars, phss, sets):
+        if set_ > 2: continue
+        t_idx = find_t_int_idx(phs)
+        sampled_kspace[phs-1, :, lin-1, par-1, set_-1] = data_ksp[set_vel[str(set_-1)]][t_idx, :, lin-1, par-1]
+
+    
+    # print('Reconstruct velocity images from k-space..')
+    # # convert back to velocity image
+    # velocity_data_sampled = {}
+    # for vel in ['u', 'v', 'w']:
+    #     velocity_data_sampled[vel] = np.zeros((N_frames, X, Y, Z), dtype = np.float32)
+    #     for t in range(N_frames):
+    #         velocity_data_sampled[vel][t], _ = fft_fcts.centered_kspace_to_velocity_img(sampled_kspace[t, :, :, :, vel_set[vel]], data['magnitude'][t], venc = data[f'venc_{vel}'][t])
+
+    # plt.imshow(velocity_data_sampled['u'][10, :, :, 60])
+    # plt.show()
+
+    print('Save..')
+    # # save to h5
+    # for vel in ['v']:
+    #     h5functions.save_to_h5(save_as, vel, velocity_data_sampled[vel], expand_dims=False)
+    #     h5functions.save_to_h5(save_as, f'venc_{vel}', data[f'venc_{vel}'], expand_dims=False)
+    
+    # TODO refactor
+    # k_space_sampled = {}
+    # for vel in ['u','v',  'w']:
+    #     k_space_sampled[vel] = sampled_kspace[:, :, :, :, vel_set[vel]]
+
+    return sampled_kspace
+
+
+
+#TBD later
+def script_coil_sens(path_datamodel):
     # 1. Use coil sensitivity matrix on CFD data
     with h5py.File(path_datamodel, mode = 'r' ) as p1: 
         spatial_res = p1['u'].shape[1:]
@@ -480,8 +560,8 @@ if __name__ == '__main__':
     coil_offset = 75  # Distance from the center of the phantom
 
     # Define the coil locations
-    coil_axis = ['x' , 'y', 'x', 'y', 'z', 'z']
-    coil_location = [coil_offset ,  coil_offset , -coil_offset, -coil_offset, coil_offset, -coil_offset]
+    coil_axis = ['x' , 'y', 'x', 'y', 'z', 'z', 'x', 'y', 'z']
+    coil_location = [coil_offset ,  coil_offset , -coil_offset, -coil_offset, coil_offset, -coil_offset, coil_offset-10, -coil_offset-10]
     coils = [define_coils(coil_radius, phantom_center, pos, ax) for pos, ax in zip(coil_location, coil_axis)]
 
     # Visualize the coils
@@ -543,17 +623,155 @@ if __name__ == '__main__':
     plt.imshow(data_sensitive['u'][ :, :, 30])
     plt.show()
 
-    # h5functions.save_to_h5(save_as, 'u_csens', data_sensitive['u'], expand_dims=False)
-    # h5functions.save_to_h5(save_as, 'v_csens', data_sensitive['v'], expand_dims=False)
-    # h5functions.save_to_h5(save_as, 'w_csens', data_sensitive['w'], expand_dims=False)
-    h5functions.save_to_h5(save_as, 'sum_coil_images', sum_coil_images, expand_dims=False)
-    # h5functions.save_to_h5(save_as, 'u', data['u'], expand_dims=False)
+    h5functions.save_to_h5(save_as, 'coil_sensitivty', sum_coil_images, expand_dims=False)
 
-    # 2. Use k-space mask on CFD data
-    # k_space_sampling_timeseries(path_kmask, path_order, path_datamodel, save_as, batchsize = 5000)
-    # k_space_sampling_static(path_kmask, path_order, path_datamodel, save_as)
+def compute_coil_sensitivity_imgs(coils,  static_mask):
+    """
+    Add coil sensitivity to velocity data
+    """
+    print('Calculate coil sensitivity matrices..')
+    spatial_res = static_mask.shape
+    coil_images = np.zeros((spatial_res[0], spatial_res[1], spatial_res[2], len(coil_axis)), dtype=np.complex128)
 
-    # 3. Reconstruct undersampled k-space with compressed sensing (CS)
+    # Compute coil sensitivity maps
+    for idx, coil in enumerate(coils):
+        sens = compute_mri_coil_sensitivity(coil, np.argwhere(static_mask), spatial_res)
+        coil_images[:,:,:,idx] =  sens.reshape(spatial_res)
+
+    return coil_images
+
+def add_coil_sensitivity(coil_images, vel, venc,  magn = None):
+
+    # compute coil images
+    N_coils = coil_images.shape[-1]
+
+    if magn is None: magn = np.ones_like(vel)
+
+    # Add coil sensitivity to velocity data
+    vel_comp = np.zeros((vel.shape[0],vel.shape[1], vel.shape[2], vel.shape[3],  N_coils), dtype=np.complex128)
+    vel_vel   = np.zeros((vel.shape[0],vel.shape[1], vel.shape[2], vel.shape[3],  N_coils), dtype=np.float64)
+    for idx in range(N_coils):
+        
+        # multiply complex image with coil sensitivity maps
+        vel_comp[:,:,:,:,idx] = np.multiply(np.multiply(magn, np.exp(1j*(vel/venc*np.pi))), np.repeat(coil_images[np.newaxis, :,:,:,idx], vel.shape[0], axis = 0))
+
+        # transform to vel image space
+        vel_vel[:, :, :,:, idx] = np.angle(vel_comp[:,:,:,:,idx])/ np.pi * venc
+
+    return vel_vel, vel_comp
+
+if __name__ == '__main__':
+    # Define datasets
+    path_kmask = 'data/kspacemask.h5'
+    path_order = 'data/order_2mm_40ms.mat'
+    path_datamodel = 'data/CARDIAC/M1_2mm_step2_static_dynamic.h5'
+    save_as = 'results/kspacesampling/'
+
+    if True: 
+        # 1. Use coil sensitivity matrix on CFD data
+        with h5py.File(path_datamodel, mode = 'r' ) as p1: 
+            spatial_res = p1['u'].shape[1:]
+            static_mask_vel = np.array(p1['mask']).squeeze()[0]
+        
+        phantom_center = [x//2 for x in spatial_res]
+        coil_radius = 30  # Radius of the coil circle
+        coil_offset = 75  # Distance from the center of the phantom
+
+        # Define the coil locations
+        coil_axis = ['x' , 'y', 'x', 'y', 'z', 'z', 'x', 'y', 'z']
+        coil_location = [coil_offset ,  coil_offset , -coil_offset, -coil_offset, coil_offset, -coil_offset, coil_offset-10, -coil_offset-10]
+        coils = [define_coils(coil_radius, phantom_center, pos, ax) for pos, ax in zip(coil_location, coil_axis)]
+
+        # # Visualize the coils
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111, projection='3d')
+        # ax.title.set_text('Coil locations in 3D space')
+
+        # for idx, coil in enumerate(coils):
+        #     ax.plot(coil[:, 0], coil[:, 1], coil[:, 2], label=f'Coil {idx+1}')
+        
+        # mask_x, mask_y, mask_z = np.where(static_mask_vel)
+        # ax.scatter(mask_x, mask_y, mask_z, c = 'gray', marker = 'o', label= 'Phantom',alpha=0.2 )
+
+        # ax.set_xlabel('X axis')
+        # ax.set_ylabel('Y axis')
+        # ax.set_zlabel('Z axis')
+        # ax.set_xlim(0, spatial_res[0]+20)
+        # ax.set_ylim(0, spatial_res[1]+20)
+        # ax.set_zlim(0, spatial_res[2]+20)
+        # plt.legend()
+        # plt.show()
+
+
+        coil_images = compute_coil_sensitivity_imgs(coils,  static_mask_vel)
+
+        data = {}
+        with h5py.File(path_datamodel, mode = 'r' ) as p1:
+            for vel in ['u', 'v', 'w']:
+                data[vel] = np.asarray(p1[vel]).squeeze()
+                data[f'venc_{vel}'] = np.asarray(p1[f'{vel}_max']).squeeze()
+
+        data_c_sens = {}
+        print('Add coil sensitivity to velocity data..')
+        for vel in ['u', 'v', 'w']:
+            # generate coil sensitivity maps
+            venc_max = np.max(data[f'venc_{vel}'])
+            data[f'{vel}_sens'], data_c_sens[vel] = add_coil_sensitivity(coil_images, data[vel], venc_max)
+
+        # save to h5
+        # h5functions.save_to_h5(save_as, 'coil_sensitivty', coil_images, expand_dims=False)
+        # h5functions.save_to_h5(save_as, 'u_csens', data[f'u_sens'], expand_dims=False)
+        # h5functions.save_to_h5(save_as, 'v_csens', data[f'v_sens'], expand_dims=False)
+        # h5functions.save_to_h5(save_as, 'w_csens', data[f'w_sens'], expand_dims=False)
+        # h5functions.save_to_h5(save_as, 'sum_coil_images', sum_coil_images, expand_dims=False)
+        # h5functions.save_to_h5(save_as, 'u', data['u'], expand_dims=False)
+
+        # 2. Use k-space mask on CFD data
+        # k_space_sampling_timeseries(path_kmask, path_order, path_datamodel, save_as, batchsize = 5000)
+        # k_space_sampling_static(path_kmask, path_order, path_datamodel, save_as)
+        t, x, y, z = data['u'].shape
+        t_res = 25
+        #make a new k-space for every coil
+        k_space_sampled_u = np.zeros((t_res, x, y, z, len(coils)), dtype = np.complex64)
+        k_space_sampled_v = np.zeros((t_res, x, y, z, len(coils)), dtype = np.complex64)
+        k_space_sampled_w = np.zeros((t_res, x, y, z, len(coils)), dtype = np.complex64)
+        for c in len(coils):
+            data_c = {}
+            data_c['u'] = data_c_sens['u'][:, :, :, :, c]
+            data_c['v'] = data_c_sens['v'][:, :, :, :, c]
+            data_c['w'] = data_c_sens['w'][:, :, :, :, c]
+            # TODO apply this for every coil
+            k_space_sampled = k_space_sampling_timeseries_new( path_order, data_c, save_as)
+            k_space_sampled_u[:, :, :, :, c] = k_space_sampled[:, :, :, :, 0]
+            k_space_sampled_v[:, :, :, :, c] = k_space_sampled[:, :, :, :, 1]
+            k_space_sampled_w[:, :, :, :, c] = k_space_sampled[:, :, :, :, 2]
+
+    
+    # 3. Reconstruct undersampled k-space with compressed sensing (CS) - save as clf file
+    # save undersampled kspace file
+
+    # save k-space data  
+    # for vel in ['u', 'v', 'w']:
+    # h5functions.save_to_h5(save_as, vel, img_space[vel], expand_dims=False)
+    #reshape to cfl standards, 
+    print(k_space_sampled_u.shape)
+    t, x, y, z, c = k_space_sampled_u.shape
+
+    reshaped_kspace =  k_space_sampled_u.transpose(1, 2, 3, 4, 0)[:, :, :, :, np.newaxis, np.newaxis, np.newaxis, np.newaxis, np.newaxis, :]
+    print(reshaped_kspace.shape)
+
+    vel = 'TEST'
+    cfl.writecfl(save_as + f'/{vel}_kspace', reshaped_kspace)
+
+
+
+    
+
+    # save sensitivity file
+
+    
+
+    
 
 
     # 4. Compare to original data
