1c86ed5bf928ef0fd555e2b7e00ba624068465c3
PiaaCaa <pia.callmer@home.se>
2024-10-14 13:45:51 +0200

    update venc script

--- pia_utils/test10_load_paired_invivo.py
+++ pia_utils/test10_load_paired_invivo.py
@@ -4,7 +4,11 @@ import h5py
 
 if __name__ == '__main__':
 
-    invivo_filenames = ['Volunteer3_4D_WholeHeart_2mm_20ms.h5']
+    invivo_filenames = ['v3_wholeheart_25mm_40ms.h5', 'v3_wholeheart_25mm_20ms.h5',
+                        'v4_wholeheart_25mm_40ms.h5', 
+                        'v5_wholeheart_25mm_40ms.h5', 'v5_wholeheart_25mm_20ms.h5',
+                        'v6_wholeheart_25mm_40ms.h5', 'v6_wholeheart_25mm_20ms.h5',
+                        'v7_wholeheart_25mm_40ms.h5', 'v7_wholeheart_25mm_20ms.h5',]
 
     data_dir = 'data/paired_invivo'
 
@@ -16,12 +20,12 @@ if __name__ == '__main__':
         # Load the lr file
         with h5py.File(f'{data_dir}/{invivo_filename}', 'r') as f:
             print(f.keys())
-            for key in f.keys():
-                print(key, f[key].shape)
+            # for key in f.keys():
+            #     print(key, f[key].shape)
 
-            print('vencs:', np.array(f['u_max']))
-            print('vencs:', np.array(f['v_max']))
-            print('vencs:', np.array(f['w_max']))
+            print('vencs:', np.array(f['u_max'][0]))
+            print('vencs:', np.array(f['v_max'][0]))
+            print('vencs:', np.array(f['w_max'][0]))
             
 
 
